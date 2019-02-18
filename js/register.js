354723bcc3caa3e9e65291143aee71f53a9e06f1
Kamil Głód <38034718+PanHungry@users.noreply.github.com>
2019-02-18 19:13:44 +0100

    Add files via upload
    js registration validation

--- js/register.js
+++ js/register.js
@@ -1,45 +1,80 @@
+var validationOK = true;
+
+////////////////////////////////////////////////////
+
+function regValidation(id, msg){
+
+  var target = document.getElementById(id);
+  wrongText = document.createElement("div");
+  wrongText.textContent = msg;
+  wrongText.setAttribute("class", `text-danger regError`)
+  target.appendChild(wrongText);
+
+  return validationOK = false;
+}
+
+
+////////////////////////////////////////////////////
+
+function validateEmail(email) {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+//////////////////////////////////////////////////////
+
+
 function validateForm() {
-    var name = document.forms["regForm"]["name"];
-    var email = document.forms["regForm"]["email"];
-    var password1 = document.forms["regForm"]["password1"];
-    var password2 = document.forms["regForm"]["password2"];
-    var response = grecaptcha.getResponse();
+  var name = document.forms["regForm"]["name"];
+  var email = document.forms["regForm"]["email"];
+  var password1 = document.forms["regForm"]["password1"];
+  var password2 = document.forms["regForm"]["password2"];
+  var response = grecaptcha.getResponse();
 
-    if (name.value == "") {
-      alert("Wpisz nazwę użytkownika!");
-      name.focus();
-      return false;
-    }
-
-    if (email.value == "") {
-      alert("Wpisz email!");
-      email.focus();
-      return false;
-    }
-
-    if (password1.value == "") {
-      alert("Wpisz hasło!");
-      password1.focus();
-      return false;
-    }
-
-    if (password2.value == "") {
-      alert("Powtórz hasło!");
-      password2.focus();
-      return false;
-    }
-
-    if (password1.value != password2.value) {
-      alert("Hasła nie są identyczne!");
-      password2.focus();
-      return false;
-    }
-
-    if(response.length == 0) {
-      alert("Potwierdź, że nie jesteś robotem!");
-      return false;
-    }
-
-    return true;
-
+  if (name.value == "") {
+    regValidation('divName', 'Wpisz nazwę!',);
+    name.focus();
   }
+
+  else if (name.value.length > 25) {
+    regValidation('divName', 'Max 25 znaków!');
+    name.focus();
+  }
+
+  if (email.value == "") {
+    regValidation('divEmail', 'Wpisz email!');
+    email.focus();
+  }
+  else if (!validateEmail(email.value)) {
+    regValidation('divEmail', 'Wpisz poprawny email!');
+    email.focus();
+  }
+
+
+  if (password1.value == "") {
+    regValidation('divPassword1', 'Wpisz hasło!');
+    password1.focus();
+  }
+
+  if (password2.value == "") {
+    regValidation('divPassword2', 'Powtórz hasło!');
+    password2.focus();
+  }
+
+  else if (password1.value != password2.value) {
+    regValidation('divPassword2', 'Hasła nie są identyczne!');
+    password1.focus();
+  }
+
+  if (response.length == 0) {
+    regValidation('divRecaptcha', 'Potwierdź, że jesteś człowiekiem!');
+  }
+
+  if (validationOK == true) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
--- register.php
+++ register.php
@@ -21,32 +21,37 @@
                 <a href="index.html" class="aRegister">Kliknij, by wrócić na stronę główną</a>
             </div>
 
-            <form name="regForm" action="/submit.php" method="post" onsubmit="return validateForm()">
+            <form name="regForm" action="#" method="post" onsubmit="return validateForm()">
 
                 <div class="form-group">
                     <label for="inputNickname">Nazwa użytkownika</label>
                     <input type="text" class="form-control" name="name" id="inputNickname"
                         placeholder="Wpisz nazwę użytkownika">
+                    <div id="divName"></div>
                 </div>
 
                 <div class="form-group">
                     <label for="inputEmail">Adres email</label>
                     <input type="email" class="form-control" name="email" id="inputEmail" placeholder="Wpisz email">
+                    <div id="divEmail"></div>
                 </div>
 
                 <div class="form-group">
                     <label for="inputPassword1">Hasło</label>
                     <input type="password" class="form-control" name="password1" id="inputPassword1"
                         placeholder="Wpisz hasło">
+                    <div id="divPassword1"></div>
                 </div>
 
                 <div class="form-group mb-4">
                     <label for="inputPassword2">Powtórz hasło</label>
                     <input type="password" class="form-control" name="password2" id="inputPassword2"
                         placeholder="Wpisz hasło">
+                    <div id="divPassword2"></div>
                 </div>
 
                     <div class="g-recaptcha" data-sitekey="6LfDQn4UAAAAALg30Vdd15-oBwf_Ahu05mZTnPdI"></div>
+                    <div id="divRecaptcha"></div>
 
                 <button type="submit" class="btn btn-light">Zarejestruj się!</button>
             </form>
